83b8a16bd5ea856b2ccc9055885db020ec22b962
jean_lc <jl_chartrand@hotmail.com>
2021-10-09 14:26:01 -0400

    full uses of the bitarrays in storage engine

--- src/Model/Logit/update_StorageEngine.jl
+++ src/Model/Logit/update_StorageEngine.jl
@@ -1,14 +1,23 @@
 function update!(se::StorageEngine{T}, x::AbstractVector{T}, sampling::AbstractVector{Int}, mo::LogitModel) where T
+    #if we are at a new point, all computed values are wrong
+    toupdate = falses(length(se.updatedInd))
     if se.x != x
-        se.x = x
-        se.updatedInd = Int[]
+        se.x[:] = x
+        se.updatedInd[:] .= false
+        toupdate[sampling] .= true
+    #if we are at the same point as in the storage engine but some observation in the sample are not already updated
+    elseif !all(se.updatedInd[sampling])
+        #bitsample contains is a bitarray with a 1 if the observation is in the sample and a 0 else
+        bitsample = falses(length(se.updatedInd))
+        bitsample[sampling] .= true
+        notuptodate = .! se.updatedInd
+        toupdate[:] = notuptodate .& bitsample
     end
-    for i in sampling
-        if !(i in se.updatedInd)
-            push!(se.updatedInd, i)
-            se.cv[:, i] = computePrecomputedVal(x, mo.data[i], mo.u)
-        end
+    #toupdateasInt contains the indexes of observation to update, isa vector of Int
+    toupdateasInt = findall(toupdate)
+    for i in toupdateasInt
+        se.cv[:, i] = computePrecomputedVal(x, mo.data[i], mo.u)
     end
-    sort!(se.updatedInd)
-    mo
+    #tell the storage engine which observations are up to date
+    se.updatedInd[sampling] .= true
 end
