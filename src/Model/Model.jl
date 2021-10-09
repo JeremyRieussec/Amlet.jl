3d1fba1cc18e30accb6849007764d62514c570d2
jean_lc <jl_chartrand@hotmail.com>
2021-10-09 15:34:44 -0400

    Update Model.jl

--- src/Model/Model.jl
+++ src/Model/Model.jl
@@ -9,10 +9,18 @@ mutable struct LogitModel{U<:IsUpdatable, D <: AbstractData} <: AmletModel{U, D}
     end
     function LogitModel(data::D; upd::Bool = false) where {D <: AbstractData}
         UPD = upd ? Updatable : NotUpdatable
+        T = Float64
         upd && return new{UPD, D}(LinUti, data, StorageEngine(data, T))
         return new{UPD, D}(LinUti, data)
     end
+    function LogitModel{isUPD}(data::D) where {D <: AbstractData, isUPD <: IsUpdatable}
+        T = Float64
+        upd = (isUPD == Updatable)
+        upd && return new{isUPD, D}(LinUti, data, StorageEngine(data, T))
+        return new{isUPD, D}(LinUti, data)
+    end
 end
+
 mutable struct MixedLogitModel{U<:IsUpdatable, D <: AbstractData} <: AmletModel{U, D}
     u::AbstractUtility
     data::D
