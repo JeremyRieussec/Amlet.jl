49c2742335a570e05c80f318c4c72bf76fd88fa8
jean_lc <jl_chartrand@hotmail.com>
2021-10-09 14:56:04 -0400

    Update ll.jl

--- src/Model/Logit/ll.jl
+++ src/Model/Logit/ll.jl
@@ -3,6 +3,7 @@ function Sofia.F(x::Vector{T}, mo::LogitModel{UPD, D};
         sample = 1:length(mo.data)) where {T, UPD, D}
     ac = zero(T)
     nind = 0
+    UPD == Updatable && @assert (mo.se.x == x && all(mo.se.updatedInd[sample])) "Storage Engine not updated" 
     for i in sample
         ns = nsim(mo.data[i])
         cv = (UPD == NotUpdatable) ? computePrecomputedVal(x, mo.data[i], mo.u) : @view mo.se.cv[:, i]
@@ -12,8 +13,27 @@ function Sofia.F(x::Vector{T}, mo::LogitModel{UPD, D};
     return -ac/nind
 end
 
-function Sofia.grad!(x::AbstractVector{T}, mo::LogitModel{UPD, D}, ac::Array{T, 1}; 
+function Sofia.Fs!(x::Vector{T}, mo::LogitModel{UPD, D}, ac::Array{T, 1}; 
+        sample = 1:length(mo.data), inplace::Bool = false) where {T, UPD, D}
+    UPD == Updatable && @assert (mo.se.x == x && all(mo.se.updatedInd[sample])) "Storage Engine not updated" 
+    for (index, i) in enumerate(sample)
+        ns = nsim(mo.data[i])
+        cv = (UPD == NotUpdatable) ? computePrecomputedVal(x, mo.data[i], mo.u) : @view mo.se.cv[:, i]
+        ll = loglogit(x, mo.data[i], mo.u, cv)
+        inplace ? ac[index] = ll : ac[i] = ll
+    end
+    return -ac
+end
+function Sofia.Fs(x::Vector{T}, mo::LogitModel{UPD, D}; 
         sample = 1:length(mo.data)) where {T, UPD, D}
+    ac = Array{T, 1}(undef, length(sample))
+    Fs!(x, mo, ac; sample = sample, inplace = false)
+    return ac
+end
+
+function Sofia.grad!(x::Vector{T}, mo::LogitModel{UPD, D}, ac::Array{T, 1}; 
+        sample = 1:length(mo.data)) where {T, UPD, D}
+    UPD == Updatable && @assert (mo.se.x == x && all(mo.se.updatedInd[sample])) "Storage Engine not updated" 
     ac[:] .= zero(T)
     nind = 0
     for i in sample
@@ -26,8 +46,36 @@ function Sofia.grad!(x::AbstractVector{T}, mo::LogitModel{UPD, D}, ac::Array{T, 
     return ac
 end
 
+function Sofia.grad(x::Vector{T}, mo::LogitModel{UPD, D}; 
+        sample = 1:length(mo.data)) where {T, UPD, D}
+        ac = Array{T, 1}(undef, length(x))
+        Sofia.grad(x, mo, ac, sample = sample)
+end
+
+function Sofia.grads!(x::Vector{T}, mo::LogitModel{UPD, D}, ac::AbstractArray{T, 2}; 
+        sample = 1:length(mo.data), inplace::Bool = true) where {T, UPD, D}
+    UPD == Updatable && @assert (mo.se.x == x && all(mo.se.updatedInd[sample])) "Storage Engine not updated" 
+    for (index, i) in enumerate(sample)
+        ns = nsim(mo.data[i])
+        cv = (UPD == NotUpdatable) ? computePrecomputedVal(x, mo.data[i], mo.u) : @view mo.se.cv[:, i]
+        gll = gradloglogit(x, mo.data[i], mo.u, cv)
+        inplace ? ac[:, index] = gll : ac[:, i] = gll
+    end
+    ac[:, :] .*= -1
+    return ac
+end
+
+function Sofia.grads(x::Vector{T}, mo::LogitModel{UPD, D}; 
+    sample = 1:length(mo.data)) where {T, UPD, D}
+    dim = length(x)
+    ac = Array{T, 2}(undef, dim, length(sample))
+    grads!(x, mo, ac, sample = sample, inplace = false)
+    return ac
+end
+
 function Sofia.H!(x::Vector{T}, mo::LogitModel{UPD, D}, ac::Array{T, 2}; 
         sample = 1:length(mo.data)) where {T, UPD, D}
+    UPD == Updatable && @assert (mo.se.x == x && all(mo.se.updatedInd[sample])) "Storage Engine not updated" 
     ac[:, :] .= zero(T)
     nind = 0
     for i in sample
@@ -42,6 +90,7 @@ end
 
 function Sofia.Hdotv!(x::AbstractVector{T}, mo::LogitModel{UPD, D}, v::AbstractVector, ac::Array{T, 1};
         sample = 1:length(mo.data)) where {T, UPD, D}
+    UPD == Updatable && @assert (mo.se.x == x && all(mo.se.updatedInd[sample])) "Storage Engine not updated" 
     ac[:] .= zero(T)
     nind = 0
     for i in sample
@@ -56,7 +105,7 @@ end
 
 function Sofia.Hdotv(x::AbstractVector{T}, mo::LogitModel{UPD, D}, v::AbstractVector;
         sample = 1:length(mo.data)) where {T, UPD, D}
-    ac = zeros(T, length(x))
+    ac = Array{T, 1}(undef, length(x))
     Hdotv!(x, mo, v, ac, sample = sample)
     return ac
 end
@@ -64,6 +113,7 @@ end
 
 function Sofia.BHHH!(x::Vector{T}, mo::LogitModel{UPD, D}, ac::Array{T, 2}; 
         sample = 1:length(mo.data)) where {T, UPD, D}
+    UPD == Updatable && @assert (mo.se.x == x && all(mo.se.updatedInd[sample])) "Storage Engine not updated" 
     dim = length(x)
     ac[:, :] .= zero(T)
     nind = 0
@@ -80,6 +130,7 @@ end
 
 function Sofia.BHHHdotv!(x::Vector{T}, mo::LogitModel{UPD, D}, v::Vector, ac::Array{T, 1}; 
         sample = 1:length(mo.data)) where {T, UPD, D}
+    UPD == Updatable && @assert (mo.se.x == x && all(mo.se.updatedInd[sample])) "Storage Engine not updated" 
     dim = length(x)
     ac[:] .= zero(T)
     nind = 0
@@ -103,38 +154,5 @@ function Sofia.BHHHdotv(x::AbstractVector{T}, mo::LogitModel{UPD, D}, v::Vector;
 end
 
 
-function Sofia.Fs!(x::Vector{T}, mo::LogitModel{UPD, D}, ac::Array{T, 1}; 
-        sample = 1:length(mo.data), inplace::Bool = false) where {T, UPD, D}
-    for (index, i) in enumerate(sample)
-        ns = nsim(mo.data[i])
-        cv = (UPD == NotUpdatable) ? computePrecomputedVal(x, mo.data[i], mo.u) : @view mo.se.cv[:, i]
-        ll = loglogit(x, mo.data[i], mo.u, cv)
-        inplace ? ac[index] = ll : ac[i] = ll
-    end
-    return -ac
-end
-function Sofia.Fs(x::Vector{T}, mo::LogitModel{UPD, D}; 
-        sample = 1:length(mo.data)) where {T, UPD, D}
-    ac = Array{T, 1}(undef, length(sample))
-    Fs!(x, mo, ac; sample = sample, inplace = false)
-    return ac
-end
-function Sofia.grads!(x::Vector{T}, mo::LogitModel{UPD, D}, ac::AbstractArray{T, 2}; 
-        sample = 1:length(mo.data), inplace::Bool = true) where {T, UPD, D}
-    for (index, i) in enumerate(sample)
-        ns = nsim(mo.data[i])
-        cv = (UPD == NotUpdatable) ? computePrecomputedVal(x, mo.data[i], mo.u) : @view mo.se.cv[:, i]
-        gll = gradloglogit(x, mo.data[i], mo.u, cv)
-        inplace ? ac[:, index] = gll : ac[:, i] = gll
-    end
-    ac[:, :] .*= -1
-    return ac
-end
 
-function Sofia.grads(x::Vector{T}, mo::LogitModel{UPD, D}; 
-    sample = 1:length(mo.data)) where {T, UPD, D}
-    dim = length(x)
-    ac = Array{T, 2}(undef, dim, length(sample))
-    grads!(x, mo, ac, sample = sample, inplace = false)
-    return ac
-end
+
