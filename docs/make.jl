92828f0625f89befc2786a84ff34eabc7a5dd557
JeremyRieussec <jeremy.rieussec@gmail.com>
2022-02-07 18:42:22 -0500

    retrieving docs

--- docs/make.jl
+++
@@ -1,6 +0,0 @@
-# push!(LOAD_PATH, "C:/Users/jerem/OneDrive/Desktop/GitHub/Code/FinalCode/Amlet.jl")
-# J' utilise ca pour travailler localement avant de push sur le gitHub pour ensuite update avec le Pkg 
-
-using Documenter, Amlet
-
-makedocs(sitename="My Documentation")
